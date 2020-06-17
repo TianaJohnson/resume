6bbd87409d5f410b95543621865d2a70becfc19c
Tiana <Tiana.marie013@gmail.com>
2020-06-16 19:55:24 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 import Main from './Main/Main';
+import Header from './Header/Header';
 
 function App() {
   return ( 
+    <>
+          <Header/>
           <Main/>  
+    </>
   );
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+
+
+export default function Main () {
+    return (
+        <div className="header-div">
+            
+        </div>
+     )
+
+};
